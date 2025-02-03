f67bdd35bd4d003c551065fdc1c2727469a19276
Samm Kohistani <sk@apples-MacBook-Pro.local>
2025-02-03 14:15:23 +0000

    add imports

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,7 +1,9 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .models import Product, HistoricalData
 from .serializers import ProductSerializer, HistoricalDataSerializer
 from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
@@ -12,8 +14,10 @@ class HistoricalDataView(APIView):
         # Query HistoricalData using the ticker
         data = HistoricalData.objects.filter(product__ticker=ticker)
 
+        print(f"🔍 Searching for historical data for {ticker}... Found: {data.exists()}")  # ✅ Debugging output
+
         if data.exists():
             serializer = HistoricalDataSerializer(data, many=True)
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response({"error": "No data found for the given ticker."}, status=status.HTTP_404_NOT_FOUND)
